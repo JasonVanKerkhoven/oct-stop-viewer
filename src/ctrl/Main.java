d179892292b1d161aea12fc4cccb4b02855bed5f
Jason Van Kerkhoven <jason.vk@outlook.com>
2017-12-24 19:16:47 -0500

    final refactor of main/fetcher

--- src/ctrl/Main.java
+++ src/ctrl/Main.java
@@ -28,6 +28,7 @@ import org.json.simple.parser.ParseException;
 //import packages
 import datatypes.BusStop;
 import exceptions.OCTException;
+import octy.OctyDisplay;
 
 
 
@@ -36,11 +37,13 @@ public abstract class Main
 	//execution
 	public static void main(String[] args) 
 	{
+		//init variables
+		StopTimeFetcher fetcher = new StopTimeFetcher();
+		OctyDisplay display = new OctyDisplay(true);
+		
 		//declaring config variables
 		boolean debug = false;
 		boolean squashEmpty = true;
-		Integer stopNum = null;
-		Integer updateFreq = null;		//call limit of 10k per day ==> 6.94 per min
 		
 		//parse parameters
 		for (String arg : args)
@@ -56,69 +59,18 @@ public abstract class Main
 					squashEmpty = false;
 					break;
 				default:
-					try
-					{
-						stopNum = Integer.parseInt(arg);
-					}
-					catch (NumberFormatException e)
-					{
-						System.out.println("StopNo must be a valid 32bit integer");
-						System.exit(0);
-					}
+					System.out.println("Unknown flag given, accepted varients: -d, -a");
+					System.exit(0);
 					break;
 			}
 		}
 		
-		if (stopNum == null)
+		//init config
+		
+		//main update loop
+		while (true)
 		{
 			
-		}
-		else
-		{
-			//declaring possible parameters
-			int stopNo = 0;
-			boolean printEmpty = true;
-			
-			try
-			{
-				if(args.length == 1)
-				{
-					printEmpty = false;
-					stopNo = Integer.parseInt(args[0]);
-				}
-				else if(args.length >= 2)
-				{
-					printEmpty = (args[0].equals("printall") || args[0].equals("true") || args[0].equals("1"));
-					stopNo = Integer.parseInt(args[1]);
-				}
-				
-				//create new fetcher and check stop num for validity
-				StopTimeFetcher fetcher = new StopTimeFetcher();
-				
-				//fetch and parse bus info
-				System.out.println("Fetching...");
-				BusStop stop = fetcher.fetchAndParseAllStopTimes(stopNo);
-				
-				//print
-				System.out.println("\n" + stop.getPrintable(printEmpty));
-			}
-			catch (NumberFormatException e)
-			{
-				System.out.println("Stop number must be integer in range 0 - " + Integer.MAX_VALUE);
-			}
-			catch (IOException e)
-			{
-				System.out.println(e.getMessage());
-				System.out.println("Cannot reach URL \"" + StopTimeFetcher.GET_ALL_BUS_TIMES_URL + "\"");
-			}
-			catch (ParseException e)
-			{
-				System.out.println(e.getMessage());	//TODO HANDLE
-			}
-			catch (OCTException e)
-			{
-				System.out.println(e.getMessage()); //TODO HANDLE
-			}
 		}
 	}
 }
